069dc621e3d097a68c698161baaf5eb411f18a88
Stanely Sitonga <stanbosh@gmail.com>
2023-07-31 14:52:26 -0700

    Memory set

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,3 @@
-main.h
-
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -13,6 +11,5 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
 #endif
 

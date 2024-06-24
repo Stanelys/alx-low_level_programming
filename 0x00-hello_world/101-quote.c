d81c622a7e63709ce1bc92077dae7785bc875e7a
root <stanbosh@gmail.com>
2024-06-24 17:01:02 +0000

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * Main-  Main- C programm that prints and that piece of art is useful" - Dora Korpar, 2015-10-19
- * return 1 - success
- */
+* Main C - prints that piece of Art is useful dora korpar 2015-10-19
+*
+* Return: Always 1(success)
+*/
 int main(void)
 {
 
-     fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; stdout);
-     return (1);
+        write(2,"and that piece of art is usefu\" - Dora Korpar, 2015-10-19\n". 59):
+        return (1);
 }
+
